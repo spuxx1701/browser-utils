e60017f03ff415537d635788d8bb817ecb684bae
@LeopoldHock <leopold.hock@deutschebahn.com>
2024-05-06 10:12:52 +0200

    chore: Remove commented config

--- vite.config.ts
+++ vite.config.ts
@@ -27,23 +27,5 @@ export default defineConfig({
       formats: ['es'],
       fileName: `index`,
     },
-    // TODO: Look for a way to get this to work so we can use nested imports paths
-    // rollupOptions: {
-    //   // This enables us to add include entrypoints besides `lib/index.ts` into the bundle.
-    //   // It leads to a better developer experience when importing functionalities from the lib
-    //   // since it allows deep imports like `import { Logger } from '@spuxx/browser-utils/services/logger';`.
-    //   input: Object.fromEntries(
-    //     glob
-    //       .sync('lib/**/*.{ts,tsx}', { ignore: '**/*.{test,spec}.{ts,tsx}' })
-    //       .map((file) => [
-    //         relative('lib', file.slice(0, file.length - extname(file).length)),
-    //         fileURLToPath(new URL(file, import.meta.url)),
-    //       ]),
-    //   ),
-    //   output: {
-    //     assetFileNames: 'assets/[name][extname]',
-    //     entryFileNames: '[name].js',
-    //   },
-    // },
   },
 });
